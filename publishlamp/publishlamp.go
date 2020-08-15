1a59d5ced016d14bae9add4bb502a268c37eddcc
Sergey Aslamazov <35179551+MrVisioo@users.noreply.github.com>
2020-08-15 19:14:14 +0300

    remove unused code

--- publishlamp/publishlamp.go
+++ publishlamp/publishlamp.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 )
 
 type Bulbs struct {
@@ -123,34 +122,5 @@ func Publish(host string, bulb_ids []string, message []byte) {
 		if p_token.Wait() && p_token.Error() != nil {
 			log.Fatal(p_token.Error())
 		}
-	}
-}
-
-func PublishCmd(host string, bulb_id string, message string) {
-	cmd_pub := exec.Command("mosquitto_pub", "-h", host, "-t", bulb_id, "-f", message)
-	log.Printf(cmd_pub.String())
-	err := cmd_pub.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Waiting to publish on %s", bulb_id)
-	err = cmd_pub.Wait()
-	log.Printf("Publish on %s is success", bulb_id)
-}
-
-func PublishmanyCmd(host string, bulb_ids []string, message string) {
-	cmds := make([]*exec.Cmd, len(bulb_ids))
-	errs := make([]error, len(bulb_ids))
-	for i, value := range bulb_ids {
-		cmds[i] = exec.Command("mosquitto_pub", "-h", host, "-t", value, "-f", message)
-		errs[i] = cmds[i].Start()
-		log.Printf("Publishing on %s, %v", value, errs[i])
-		if errs[i] != nil {
-			log.Fatal(errs[i])
-		}
-	}
-	for i, value := range bulb_ids {
-		errs[i] = cmds[i].Wait()
-		log.Printf("Publish on %s is success, %v", value, errs[i])
 	}
 }
